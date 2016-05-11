72a720bdb3ab65ba77e11b8c4cb255d9e96ec3b2
Tomasz Cugowski <tomcug@gmail.com>
2016-05-11 15:43:27 +0200

    Font class rename

--- src/main/java/tletters/font/Font.java
+++ src/main/java/tletters/font/Font.java
@@ -1,53 +1,50 @@
 package tletters.font;
 
-import java.awt.*;
+import java.awt.Font;
+import java.awt.FontFormatException;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.*;
-
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 /**
  * Created by mdomagala on 4/25/16.
  */
-public class Font {
+public class FontChecker {
 
-    static boolean checkFont(String name) throws IOException, FontFormatException {
-        java.awt.Font font = java.awt.Font.createFont(java.awt.Font.TRUETYPE_FONT, new FileInputStream("src/main/resources/fonts/" + name));
-        char [] specialChars = {'ą', 'Ą', 'ć', 'Ć', 'ę', 'Ę', 'ł', 'Ł', 'ń', 'Ń', 'ó', 'Ó', 'ś', 'Ś', 'ź', 'Ź', 'ż', 'Ż', 'A', 'a','1'};
+    public static final char[] SPECIAL_CHARS = {'ą', 'Ą', 'ć', 'Ć', 'ę', 'Ę', 'ł', 'Ł', 'ń', 'Ń', 'ó', 'Ó', 'ś', 'Ś', 'ź', 'Ź', 'ż', 'Ż'};
 
-        for (char c:specialChars) {
-            if(!font.canDisplay(c)){
+    public static boolean checkFont(String name) throws IOException, FontFormatException {
+        Font font = Font.createFont(Font.TRUETYPE_FONT, new FileInputStream("src/main/resources/fonts/" + name));
+        for (char c : SPECIAL_CHARS) {
+            if (!font.canDisplay(c)) {
                 return false;
             }
         }
         return true;
     }
 
-    static void checkAllFonts() throws IOException, FontFormatException {
+    public static void checkAllFonts() throws IOException, FontFormatException {
         final String newPath = "src/main/resources/fontsWithPolishCharacters/";
         final String startPath = "src/main/resources/fonts/";
         Path destination, source;
         String fileName;
-
-        if(!Files.exists(Paths.get(newPath))){
+        if (!Files.exists(Paths.get(newPath))) {
             Files.createDirectory(Paths.get(newPath));
         }
-
-        final DirectoryStream<Path> paths = Files.newDirectoryStream(Paths.get(startPath), new DirectoryStream.Filter<Path>(){
-            @Override
-            public boolean accept(Path entry) throws IOException {
-                return entry.getFileName().toString().endsWith(".ttf") || entry.getFileName().toString().endsWith(".otf");
-            }
-        });
-
+        final DirectoryStream<Path> paths = Files.newDirectoryStream(Paths.get(startPath),
+                (Path entry) -> entry.getFileName().toString().endsWith(".ttf") || entry.getFileName().toString().endsWith(".otf"));
         for (Path path : paths) {
             fileName = path.getFileName().toString();
-
-            if(checkFont(fileName)){
+            if (checkFont(fileName)) {
                 source = Paths.get(startPath + fileName);
                 destination = Paths.get(newPath + fileName);
                 Files.copy(source, destination, StandardCopyOption.REPLACE_EXISTING);
             }
         }
     }
+
 }
