bbc14fe8eb33d668705fd2c3c57555319473eef1
polarny <sta.dawidowicz@gmail.com>
2016-05-11 20:32:04 +0200

    Feature Extractor now saves to a property file with a given directory. Creates a file if it does not exist.

--- src/main/java/FeatureExtraction/ExtractionAlgorithm.java
+++ src/main/java/FeatureExtraction/ExtractionAlgorithm.java
@@ -1,4 +1,4 @@
-package FeatureExtraction;
+package featureextraction;
 
 import java.awt.image.BufferedImage;
 
--- src/main/java/FeatureExtraction/FeaturesExtractor.java
+++ src/main/java/FeatureExtraction/FeaturesExtractor.java
@@ -1,25 +1,54 @@
-package FeatureExtraction;
+package featureextraction;
 
 import java.awt.image.BufferedImage;
+import java.io.*;
 import java.util.Properties;
 
 public class FeaturesExtractor {
+    private static final String defaultDir = "defaultProperties";
     private Properties properties;
+    private String directory;
 
-    public FeaturesExtractor(){
+
+    public FeaturesExtractor() throws IOException {
         properties = new Properties();
+        directory = defaultDir;
+        try (FileInputStream in = new FileInputStream(directory)) {
+            properties.load(in);
+        } catch (FileNotFoundException e) {
+            new File(directory);
+            FileInputStream in = new FileInputStream(directory);
+            properties.load(in);
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public FeaturesExtractor(Properties properties) {
-        this.properties = properties;
+    public FeaturesExtractor(String directory) throws IOException {
+        this.directory = directory;
+        properties = new Properties();
+        try (FileInputStream in = new FileInputStream(directory)) {
+            properties.load(in);
+        } catch (FileNotFoundException e) {
+            new File(directory);
+            FileInputStream in = new FileInputStream(directory);
+            properties.load(in);
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void extractFeatures(ExtractionAlgorithm algorithm, BufferedImage image, String name){
+    public void extractFeatures(ExtractionAlgorithm algorithm, BufferedImage image, String name) throws IOException {
         Double[] features = algorithm.extractFeatures(image);
         saveFeatures(name, features);
     }
 
-    public void saveFeatures(String name , Double[] features){
+    public void saveFeatures(String name , Double[] features) throws IOException {
         properties.put(name, features);
+        FileOutputStream out = new FileOutputStream(directory);
+        properties.store(out, null);
+        out.close();
     }
 }
