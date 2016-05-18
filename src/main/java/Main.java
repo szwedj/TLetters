b2302671b434e23d2762d02b9e6e69e46ce00b3f
Tomasz Cugowski <tomcug@gmail.com>
2016-05-18 14:29:42 +0200

    Code cleanup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,9 @@
-import tletters.featureextraction.FeaturesExtractor;
-
-import java.io.IOException;
-
 /**
  * Created by mariuszbeltowski on 23/04/16.
  */
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        FeaturesExtractor featuresExtractor = new FeaturesExtractor("xxxxxxxxxxxxx");
+        System.out.println("Hello World!");
     }
 }
